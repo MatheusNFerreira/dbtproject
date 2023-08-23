d316fa1d96252d34a451798e808b3b0c5a3aa069
Matheus Nunes Ferreira <141282048+MatheusNFerreira@users.noreply.github.com>
2023-08-23 12:56:23 -0300

    Update employees.sql

--- models/employees.sql
+++ models/employees.sql
@@ -2,7 +2,7 @@ with calc_employees as (
     select
     date_part(year, current_date) - date_part(year, birth_date) age,
     date_part(year, current_date) - date_part(year,hire_date) lengthofservice,
-    firt_name || ' ' || last_name name, *
+    first_name || ' ' || last_name name, *
     from {{source('sources','employees')}}
 )
 select * from calc_employees
